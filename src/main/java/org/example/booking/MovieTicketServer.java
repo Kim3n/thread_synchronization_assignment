0de61bd099e424a47484266efc052a2bf78466dc
Kim <lademokim@hotmail.com>
2023-02-21 13:53:52 +0100

    Finished MovieTicketServer

--- src/main/java/org/example/booking/MovieTicketServer.java
+++ src/main/java/org/example/booking/MovieTicketServer.java
@@ -1,19 +1,24 @@
 package org.example.booking;
 
 public class MovieTicketServer {
-    // TBD
+    private String movieName;
+    private int availableSeats;
 
-    public MovieTicketServer() {
+    public MovieTicketServer(String movieName, int availableSeats) {
+        this.movieName = movieName;
+        this.availableSeats = availableSeats;
+
         // TBD
     }
 
-    public void bookTicket() {
-        // TBD
+    synchronized void bookTicket(String customerName, int numberOfSeats) {
+        System.out.println("Hi," + customerName + " : " + availableSeats + " : Seats available for " + movieName);
 
         if ((availableSeats - numberOfSeats) < 0) {
-            // TBD
+            System.out.println("Hi," + customerName + " : Seats not available for " + movieName);
+        } else {
+            availableSeats = availableSeats - numberOfSeats;
+            System.out.println("Hi," + customerName + " : " + numberOfSeats + " Seats booked successfully for" + movieName);
         }
-
-        // TBD
     }
 }
